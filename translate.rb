14e52de9dfcf3ba22762c72f1b7a7ec4cde3683d
Ceil <ceilwoo@gmail.com>
2015-11-06 12:37:49 +0800

    Update translate.rb
    v1.1 add voice for mac os, add '-v' to set actived
            support short sentence

--- translate.rb
+++ translate.rb
@@ -1,47 +1,78 @@
-# terminal translator 1.0 (en <-> chs)
-# 命令行中英翻译脚本
+# terminal translator (en <-> chs)
+# 命令行中英翻译机
 # by ceil 20150905
+# ----------------
+# v1.1 add voice for mac os, add '-v' to active, Example: ruby translate.rb ceil -v
+#      support short sentence
+# v1.0 20150905
 
 require 'net/http'
 require 'uri'
 require 'json'
 
+############################################
 # KEY申请:  http://fanyi.youdao.com/openapi
+############################################
 key = "YOUR_KEY"
 keyfrom = "YOUR_KEYFROM"
 
-word = ARGV[0]
-output_phonetic = ""
-output_explains = ""
+############################################
+# Main function
+############################################
+@enable_voide = false
+@include_chinese = false
+@debug_mode = false
 
-if word == nil
-	puts "[Query require]"
+argv_list = []
+ARGV.each do |x|
+	argv_list << x if x != "-v" and x != "-d"
+	@enable_voide = true if x == "-v"
+	@debug_mode = true if x == "-d"
+end
+
+word = argv_list.join(" ")
+
+@include_chinese = true if (word =~ /\p{Han}/) != nil
+
+if word == ""
+	puts "[Parameter Require]"
 	exit
 end
 
-uri =uri = URI.parse(URI.encode("http://fanyi.youdao.com/openapi.do?keyfrom=#{keyfrom}&key=#{key}&type=data&doctype=json&version=1.1&q=#{word}"))
+output_phonetic = ""
+output_explains = ""
+
+uri = URI.parse(URI.encode("http://fanyi.youdao.com/openapi.do?keyfrom=#{keyfrom}&key=#{key}&type=data&doctype=json&version=1.1&q=#{word}"))
+
+puts "translating..."
+
 http = Net::HTTP.new(uri.host, uri.port)
 res_json = JSON.parse(http.request(Net::HTTP::Get.new(uri.request_uri)).body)
 
+p res_json if @debug_mode
+
 begin
 	puts "\n"
-	puts "[query]".ljust(11) + res_json["query"]
-	
+	puts "[query]".ljust(13) + res_json["query"]
+	puts "[translation]".ljust(13) + res_json["translation"].join(" ") if res_json["translation"]
 	if res_json["basic"]
 		res_json["basic"].each do |k,v|
 			output_phonetic += "(#{k}) #{v}" + ";" if k.include?("phonetic")
-			output_explains = "[#{k}]".ljust(11) +  res_json["basic"]["explains"].join("\n           ") if k == "explains"
+			output_explains = "[#{k}]".ljust(13) +  res_json["basic"]["explains"].join("\n             ") if k == "explains"
 		end
 	end
-	
+
 	puts output_explains
-	print "[Web]".ljust(11)
-	res_json["web"].each do |f|
-		print f["key"] + ": " + f["value"].join(", ") + "\n           "
+	print "[Web]".ljust(13)
+	if res_json["web"]
+		res_json["web"].each do |f|
+			print f["key"] + ": " + f["value"].join(", ") + "\n             "
+		end
 	end
 	puts "\n"
 	puts output_phonetic
 	puts "\n"
+	system "say #{word}" if !@include_chinese and @enable_voide 
 rescue => e
 	puts res_json.to_json
 	puts "\n"
